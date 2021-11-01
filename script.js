c3e19cb4114e8afbb65fa183355e7bfaf1659e38
Serebriakova <serebriakova.alla@gmail.com>
2021-11-01 17:21:07 +0200

    update

--- script.js
+++ script.js
@@ -2,20 +2,21 @@ const arr = [1, 2, 3, -1, -2, -3];
 
 
 function positive (arr) {
-    const arrPositive = [];
+    
     if (arr.length === 0) {
         return alert("Array is empty");
     }
+    const arrPositive = [];
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] > 0) {
             arrPositive.push(arr[i]);
-        } else { continue; }
+        } 
     }
 if (arrPositive.length === 0) {
-    return console.log(null);
+    return null;
 }
-return console.log(arrPositive);
+return arrPositive;
 
 }
 
-positive (arr);
+console.log(positive (arr));
